84ef0b6ac1fa1348d515206d441783d9aa9d0541
070boroda <zely1984@gmail.com>
2019-07-11 21:08:12 +0300

    Initial commit

---
+++ src/One.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class One {
+    public static void main(String[] args) {
+
+        double a,b,c;
+        double result;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ведите а ");
+        a = in.nextDouble();
+        System.out.println("Ведите b ");
+        b = in.nextDouble();
+        System.out.println("Ведите c ");
+        c = in.nextDouble();
+        System.out.println("Вы вели a=" + a +"\nВы вели b="+ b +"\nВы вели c=" + c);
+        result = ((a-3)*b/2)+c;
+        System.out.println("Ответ ((a-3)*b/2)+c=" + result );
+    }
+}
---
+++ src/Two.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Two {
+
+    public static void main(String[] args) {
+
+        double a,b,c;
+        double result;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ведите а ");
+        a = in.nextDouble();
+        System.out.println("Ведите b ");
+        b = in.nextDouble();
+        System.out.println("Ведите c ");
+        c = in.nextDouble();
+        System.out.println("Вы вели a=" + a +"\nВы вели b="+ b +"\nВы вели c=" + c);
+        result = ((b+(Math.sqrt(Math.pow(b,2)+4*a*c))/2*a))-Math.pow(a,3)*c+Math.pow(b,-2);
+        System.out.println("Ответ " + result );
+    }
+}
