392eeb35ed6ad02b57da0cc2a903d3503a41b708
070boroda <zely1984@gmail.com>
2019-07-16 21:11:04 +0300

    add five task

--- src/by/epam/zelenko/branchingprogram/Five.java
+++ src/by/epam/zelenko/branchingprogram/Five.java
@@ -1,4 +1,14 @@
 package by.epam.zelenko.branchingprogram;
 
 public class Five {
+
+    public static void main(String[] args) {
+
+        int x = 8;
+
+        if(x<=3){
+            System.out.println(Math.pow(x,2)-3*x+9);
+        } else if(x>3)
+            System.out.println(1/(Math.pow(x,3)+6));
+    }
 }
