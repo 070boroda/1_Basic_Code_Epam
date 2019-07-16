438123f67f878cfc8e5fcbeaec0ff03f776d9540
070boroda <zely1984@gmail.com>
2019-07-16 20:58:44 +0300

    add five task

---
+++ src/by/epam/zelenko/branchingprogram/Five.java
@@ -0,0 +1,4 @@
+package by.epam.zelenko.branchingprogram;
+
+public class Five {
+}
--- src/by/epam/zelenko/branchingprogram/Four.java
+++ src/by/epam/zelenko/branchingprogram/Four.java
@@ -1,4 +1,24 @@
 package by.epam.zelenko.branchingprogram;
 
 public class Four {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int x = 8;
+        int y = 4;
+        int z = 11;
+
+        if((x <=a && y<=b) || (y<=a && x<=b)){
+            System.out.println("true");
+        } else if((x <=a && z<=b) || (z<=a && x<=b)) {
+            System.out.println("true");
+        } else if((y <=a && z<=b) || (z<=a && y<=b)){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
 }
