47cfd7e487394d8c9b1fc8984b1fd2837a4461bf
070boroda <zely1984@gmail.com>
2019-07-16 21:26:26 +0300

    add Two class cycleprogram

--- src/by/epam/zelenko/cycleprogram/One.java
+++ src/by/epam/zelenko/cycleprogram/One.java
@@ -1,7 +1,24 @@
 package by.epam.zelenko.cycleprogram;
 
+import java.util.Scanner;
+
 public class One {
     public static void main(String[] args) {
 
+        int x;
+        int count = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Enter number");
+        }
+        x = scanner.nextInt();
+
+        for (int i=1; i<=x;i++){
+           count+=i;
+        }
+        System.out.println(count);
     }
 }
---
+++ src/by/epam/zelenko/cycleprogram/Two.java
@@ -0,0 +1,8 @@
+package by.epam.zelenko.cycleprogram;
+
+public class Two {
+
+    public static void main(String[] args) {
+
+    }
+}
