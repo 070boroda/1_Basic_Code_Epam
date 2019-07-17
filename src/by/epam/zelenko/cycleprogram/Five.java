35885a0e04528eee787728521e071ce93a71b679
070boroda <zely1984@gmail.com>
2019-07-17 21:27:33 +0300

    add Six task cycleprogram

---
+++ src/by/epam/zelenko/cycleprogram/Five.java
@@ -0,0 +1,12 @@
+package by.epam.zelenko.cycleprogram;
+
+public class Five {
+
+    public static void main(String[] args) {
+
+        for(char i = '\u0041'; i <= '\u007a'; i++){
+
+            System.out.print((int)i + "=" + i + " ");
+        }
+    }
+}
--- src/by/epam/zelenko/cycleprogram/Six.java
+++ src/by/epam/zelenko/cycleprogram/Six.java
@@ -1,12 +1,27 @@
 package by.epam.zelenko.cycleprogram;
 
+import java.util.Scanner;
+
 public class Six {
 
     public static void main(String[] args) {
 
-        for(char i = '\u0041'; i <= '\u007a'; i++){
+        int m;
+        int n;
 
-            System.out.print((int)i + "=" + i + " ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter m and n");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Enter m and n");
+            scanner.next();
+        }
+        m = scanner.nextInt();
+        n = scanner.nextInt();
+
+        for (int i=n; i < m; i++) {
+            if (m % i == 0) {
+                System.out.println(m / i);
+            }
         }
     }
 }
