e667a9fcff90e69e3d3085a9cf20ffc40da9e980
070boroda <zely1984@gmail.com>
2019-07-17 20:24:39 +0300

    add Two class cycleprogram

--- src/by/epam/zelenko/cycleprogram/Two.java
+++ src/by/epam/zelenko/cycleprogram/Two.java
@@ -4,5 +4,27 @@ public class Two {
 
     public static void main(String[] args) {
 
+        int a;
+        int b;
+        double y;
+        double x;
+        double h;
+
+        a=-6;
+        b=-4;
+        h=0.1;
+        for (x=a;x<=b;x+=h )
+        {
+            System.out.print("x= " +x+",");
+            if (x>2){
+                y = x;
+                System.out.println("y ="+y);
+            }
+            else if (x<=-2){
+                y =-x;
+                System.out.println("y ="+y);
+
+            }
+        }
     }
 }
