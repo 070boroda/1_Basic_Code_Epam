d45577aa35c6d27177d33aeefd618d8e6bd8d1b6
boroda <zely1984@gmail.com>
2019-07-18 15:50:18 +0300

    refactor

--- src/by/epam/zelenko/cycleprogram/Seven.java
+++ src/by/epam/zelenko/cycleprogram/Seven.java
@@ -9,6 +9,9 @@ public class Seven {
         int  a;
         int  b;
         int c=10;
+        int [] a1 = new int[10];
+        int [] a2;
+        int j =0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two values");
@@ -20,9 +23,14 @@ public class Seven {
         b=scanner.nextInt();
 
         while (a%c!=0) {
-            System.out.print(a % c + " ");
+            a1[j++] = a%c;
             a = a / c;
         }
+
+        for (int i = 0; i<a1.length-1;i++){
+            System.out.println(a1[i]);
+        }
+
         System.out.println();
         while (b%c!=0) {
             System.out.print(b % c + " ");
